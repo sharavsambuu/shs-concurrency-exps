c0f8520fcad52ff5ba0b691a05fbb43be591d381
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2021-09-29 00:53:52 +0800

    spawned game room server as fiber primitive.

--- python-exps/fiber_game_server.py
+++ python-exps/fiber_game_server.py
@@ -8,28 +8,62 @@ import time
 import signal
 import os
 import sys
+import uu
+import uuid
+import asyncio
 import multiprocessing
+
 
 
 def init():
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
 
+
+# тоглоомын өрөөны логик
+async def game_room_fiber():
+    room_id         = uuid.uuid4()
+    is_room_running = True
+    print(f"#ROOM_{room_id} : room is started")
+
+    while is_room_running:
+        print(f"#ROOM_{room_id} : working...")
+        await asyncio.sleep(3.01)
+
+    pass
+
+
 # дотроо тоглоомын өрөөнүүд ажиллуулж байх нэг процесс
 def worker_process(queue_in, queue_out):
     process_id = os.getpid()
     print(f"#WORKER_{process_id} : process is spawned")
-    while (True):
-        while not queue_in.empty():
-            msg = queue_in.get()
-            if msg.get("COMMAND")=="PING_WORKER":
-                print(f"#WORKER_{process_id} : received PING_WORKER command")
-                queue_out.put({"MESSAGE": "PONG", "BODY": f"{process_id}"})
 
-        time.sleep(0.01)
+    # 2 ширхэг өрөө
+    game_room_fibers = [asyncio.Task(game_room_fiber()) for i in range(1)]
 
-        pass
+    async def worker_fiber():
+        while (True):
+            while not queue_in.empty():
+                msg = queue_in.get()
+                if msg.get("COMMAND")=="PING_WORKER":
+                    print(f"#WORKER_{process_id} : received PING_WORKER command")
+                    queue_out.put({"MESSAGE": "PONG", "BODY": f"{process_id}"})
+            await asyncio.sleep(0.0)
+
+    async def fiber_cycler():
+        while True:
+            await worker_fiber()
+            for game_room in game_room_fibers:
+                await game_room
+            await asyncio.sleep(0)
+
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(fiber_cycler())
+
     pass
+
+
 
 # worker процессүүдрүү комманд илгээж удирдах зориулалттай процесс
 def admin_process(worker_queue_list):
@@ -37,7 +71,7 @@ def admin_process(worker_queue_list):
     start_time = time.time()
 
     command_condition_traps = {
-        "PING_TO_WORKERS" : [5, False]
+        "PING_TO_WORKERS" : [15, False]
     }
 
     def ping_workers():
@@ -62,15 +96,16 @@ def admin_process(worker_queue_list):
         if command_condition_traps["PING_TO_WORKERS"][0]<=elapsed_time and \
             not command_condition_traps["PING_TO_WORKERS"][1]:
             command_condition_traps["PING_TO_WORKERS"][1] = True
-            ping_workers()
             print("#ADMIN : Pinging to worker processes...")
-
-        if (int(elapsed_time)%10==0) and (command_condition_traps["PING_TO_WORKERS"][1]==True):
-            print("#ADMIN : Periodic workers health check")
             ping_workers()
+
+        #if (int(elapsed_time)%10==0) and (command_condition_traps["PING_TO_WORKERS"][1]==True):
+        #    print("#ADMIN : Periodic workers health check")
+        #    ping_workers()
 
         time.sleep(0.01)
         pass
+
     pass
 
 
@@ -78,6 +113,8 @@ def admin_process(worker_queue_list):
 # тоглоомын клиэнтийг төсөөлж төлөөлүүлсэн процесс
 def game_client():
     pass
+
+
 
 # админ удирдлага хийх зориулалттай төсөөллийн клиэнт
 def admin_client():
@@ -94,6 +131,7 @@ if __name__=="__main__":
     manager = multiprocessing.Manager()
 
     try:
+        # тоглоомын сервер ажиллуулах зориулалттай процессүүд
         worker_queue_list = []
         for i in range(N_WORKERS):
             worker_queue_in  = manager.Queue()
@@ -101,13 +139,14 @@ if __name__=="__main__":
             worker_queue_list.append((worker_queue_in, worker_queue_out))
             pool.apply_async(worker_process, (worker_queue_in, worker_queue_out,))
 
+        # бусад процессүүдээ удирдах зориулалттай админ процесс
         pool.apply_async(admin_process, (worker_queue_list,))
 
         time.sleep(1)
-
         pool.close()
         pool.join()
     except KeyboardInterrupt:
         pool.terminate()
         pool.join()
+
     pass
