35aaaf88406afc037e3c6ded9b7bb5569244d2fe
sharavsambuu gunchinish <sharavsambuu@gmail.com>
2021-10-01 23:06:26 +0800

    fibers on multiple thread with job stealing mode, which is little bit efficient because of a fewer fiber migration between processor cores.

--- cpp-exps/src/hello-fibers/fiber_stealing_on_threads.cpp
+++ cpp-exps/src/hello-fibers/fiber_stealing_on_threads.cpp
@@ -1,6 +1,114 @@
+/*
+    Олон fiber-үүдийг олон thread-ээр хувааж ажиллуулах.
+
+    work stealing горимоор fiber-уудыг ажиллуулах өөрөөр хэлбэл шинээр
+    fiber үүсгэх бүр ажилуулах thread цохон оноох тул процессорын цөм
+    хооронд fiber-ууд зөөх нь бага гэсэн үг.
+
+    https://www.boost.org/doc/libs/1_77_0/libs/fiber/doc/html/fiber/worker.html
+*/
+
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <string>
+#include <sstream>
+#include <chrono>
+#include <condition_variable>
+#include <boost/fiber/all.hpp>
+#include <boost/fiber/barrier.hpp>
+
+using namespace std::chrono_literals;
+
+static std::size_t fiber_count{0};
+static std::mutex mutex_count{};
+typedef std::unique_lock<std::mutex> lock_type;
+static boost::fibers::condition_variable_any condition_count{};
+
+
+
+void worker_fiber(char me) {
+    std::thread::id thread_id = std::this_thread::get_id();
+    {
+        std::ostringstream buffer;
+        buffer<<"fiber "<<me<<" is started on thread "<<thread_id<<"\n";
+        std::cout<<buffer.str()<<std::flush;
+    }
+
+    for (int i=0; i<100; ++i) {
+        boost::this_fiber::yield();
+
+        // энэ fiber-ийг өөр thread дээр шилжүүлсэн эсэхийг илрүүлэх
+        std::thread::id new_thread_id = std::this_thread::get_id();
+        if (new_thread_id!=thread_id) {
+            thread_id = new_thread_id;
+            std::ostringstream buffer;
+            buffer<<"fiber "<<me<<" is switched to thread "<<thread_id<<"\n";
+            std::cout<<buffer.str()<<std::flush;
+        }
+    }
+
+    lock_type lock(mutex_count);
+    // fiber ажиллах дуус бүрт fiber_count-н утгыг бууруулах
+    if (0==--fiber_count) {
+        lock.unlock();
+        condition_count.notify_all();
+    }
+}
+
+
+void worker_thread() {
+    std::ostringstream buffer;
+    buffer<<"thread "<<std::this_thread::get_id()<<" is started"<<std::endl;
+    std::cout<<buffer.str()<<std::flush;
+
+    boost::fibers::use_scheduling_algorithm<boost::fibers::algo::work_stealing>(4);
+
+    lock_type lock(mutex_count);
+    condition_count.wait(lock, []() {
+        return 0==fiber_count;
+    });
+}
+
 
 int main() {
-    std::cout<<"job stealing"<<std::endl;
+    std::cout<<"main thread "<<std::this_thread::get_id()<<" is started"<<std::endl;
+
+
+    for (char c : std::string("abcdefghijklmnopqrstuvwxyz")) {
+        boost::fibers::fiber([c]() {
+            worker_fiber(c);
+        }).detach();
+        ++fiber_count;
+    }
+
+    // хоорондоо ажил хувааж хийх боломжтой thread-үүд асаах
+    std::thread threads[] = {
+        std::thread(worker_thread),
+        std::thread(worker_thread),
+        std::thread(worker_thread)
+    };
+
+    boost::fibers::use_scheduling_algorithm<boost::fibers::algo::work_stealing>(4);
+
+    {
+        lock_type lock(mutex_count);
+        /*
+        main fiber-г suspend хийлгээд worker fiber-г ажиллагаанд оруулна
+        хэрэв дараах нөхцөл биелбэл main fiber дээр resume хийгдэнэ
+        өөрөөр хэлбэл бүх fiber-ууд ажиллаж дууссан гэсэн үг
+        */
+        condition_count.wait(lock, []() {
+            return 0==fiber_count;
+        });
+    }
+    // дээрхи lock release хийгдсэний дараа л thread-үүдээ join хийхгүй бол
+    // бусад thread-үүд condition_count дотор түгжигдэж deadlock хийгдэх боломжтой
+    for (std::thread & t : threads) {
+        t.join();
+    }
+
+    std::cout<<"done, bye."<<std::endl;
+
     return 0;
 }
