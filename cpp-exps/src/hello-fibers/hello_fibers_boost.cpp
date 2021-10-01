8941edacc25563f930a66d9925e488a9b23201f0
sharavsambuu gunchinish <sharavsambuu@gmail.com>
2021-10-01 19:55:29 +0800

    hello function based fiber.

--- cpp-exps/src/hello-fibers/hello_fibers_boost.cpp
+++ cpp-exps/src/hello-fibers/hello_fibers_boost.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <boost/fiber/all.hpp>
 
+void fn(std::string const& str, int n) {
+    for (int i=0; i<n; ++i) {
+        std::cout<<str<<" : "<<i<<std::endl;
+        boost::this_fiber::yield();
+    }
+}
+
+
 int main(int argc, char const *argv[]) {
     auto fiber = boost::fibers::fiber([] () {
-        std::cout << "Hello World" << std::endl;
+        std::cout << "Hello World from Boost::Fiber" << std::endl;
     });
     fiber.join();
+
+    boost::fibers::fiber f1(fn, "hello_fiber", 5);
+    f1.join();
+
+    std::cout<<"done."<<std::endl;
+
     return 0;
 }
