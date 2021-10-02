a3c84691608a9573188b1417df310ec7a85833f7
sharavsambuu gunchinish <sharavsambuu@gmail.com>
2021-10-03 02:04:59 +0800

    joinable coroutine

--- cpp-exps/src/hello-coroutines/hello_coop.cpp
+++ cpp-exps/src/hello-coroutines/hello_coop.cpp
@@ -3,20 +3,24 @@
 #include <coop/task.hpp>
 #include <thread>
 
-coop::task_t<> hello_coroutine()
+coop::task_t<void, true> joinable_coroutine()
 {
     auto thread_id = std::this_thread::get_id();
     std::cout<<"coroutine running on thread_"<<thread_id<<std::endl;
 
     co_await coop::suspend();
 
-    std::this_thread::sleep_for(std::chrono::milliseconds{50});
+    std::this_thread::sleep_for(std::chrono::milliseconds{2000});
+
+    std::cout<<"joinable coroutine is finished"<<std::endl;
 }
 
 int main() {
     auto my_thread = std::thread([] {
         std::cout<<"hello from thread"<<std::endl;
-        hello_coroutine();
+        auto task = joinable_coroutine();
+        task.join();
+        std::cout<<"this thread is finished to run coroutines"<<std::endl;
     });
     my_thread.join();
     std::cout<<"Hello from Main thread"<<std::endl;
