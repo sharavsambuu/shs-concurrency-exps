5988e38f3b4e7b2a459b901dfa31e37da5c1f4c7
sharavsambuu gunchinish <sharavsambuu@gmail.com>
2021-10-03 07:59:53 +0800

    little bit join-ish using while on hello coroutines.

--- cpp-exps/src/hello-coroutines/hello_coroutines.cpp
+++ cpp-exps/src/hello-coroutines/hello_coroutines.cpp
@@ -2,7 +2,8 @@
 #include <coroutine>
 #include <exception>
 #include <iostream>
-
+#include <thread>
+#include <chrono>
 
 struct HelloWorldCoro {
     struct promise_type { // compiler looks for `promise_type`
@@ -14,21 +15,37 @@ struct HelloWorldCoro {
     };
     HelloWorldCoro(promise_type* p) : m_handle(std::coroutine_handle<promise_type>::from_promise(*p)) {}
     ~HelloWorldCoro() { m_handle.destroy(); }
+    void resume() { m_handle.resume(); }
+    bool done() const noexcept { return m_handle.done(); }
     std::coroutine_handle<promise_type> m_handle;
 };
 
-HelloWorldCoro hello_coroutine() {
-    std::cout << "Hello" << std::endl;
-    co_await std::suspend_always{};
-    std::cout << "World" << std::endl;
+HelloWorldCoro hello_coroutine(const std::string name, int n) {
+    for (int i=0; i<n; ++i) {
+        std::cout<<name<<"   : call "<<i<<std::endl;
+        co_await std::suspend_always{};
+    }
+}
+
+HelloWorldCoro another_coroutine() {
+    for (int i=0; i<20; ++i) {
+        std::cout<<"another : call "<<i<<std::endl;
+        co_await std::suspend_always{};
+    }
 }
 
 int main() {
-    HelloWorldCoro my_coroutine = hello_coroutine();
-    std::cout << "first coroutine call :" << std::endl;
-    my_coroutine.m_handle.resume();
-    std::cout << "second coroutine call :" << std::endl;
-    my_coroutine.m_handle.resume();
+    std::cout<<"hello from main"<<std::endl;
+
+    HelloWorldCoro coro1   = hello_coroutine("coro1", 4);
+    HelloWorldCoro coro2   = hello_coroutine("coro2", 9);
+    HelloWorldCoro another = another_coroutine();
+    do { 
+        if (!coro1.done())   coro1.resume();
+        if (!coro2.done())   coro2.resume();
+        if (!another.done()) another.resume();
+    } while(!coro1.done() || !coro2.done() || !another.done());
     
+
     return 0;
 }
