8812d510948400ca057d84303738c96e42ca5e78
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2023-09-14 02:27:05 +0800

    using fiber scheduler in the app loop is somehow bad idea, gives segmentation fault error.

--- cpp-exps/src/hello-fibers/fiber_stealing_on_threads.cpp
+++ cpp-exps/src/hello-fibers/fiber_stealing_on_threads.cpp
@@ -20,69 +20,72 @@
 
 using namespace std::chrono_literals;
 
+typedef std::unique_lock<std::mutex> lock_type;
 static std::size_t fiber_count{0};
 static std::mutex mutex_count{};
-typedef std::unique_lock<std::mutex> lock_type;
 static boost::fibers::condition_variable_any condition_count{};
 
 
 
-void worker_fiber(char me) {
+void worker_fiber(char me)
+{
     std::thread::id thread_id = std::this_thread::get_id();
     {
         std::ostringstream buffer;
-        buffer<<"fiber "<<me<<" is started on thread "<<thread_id<<"\n";
-        std::cout<<buffer.str()<<std::flush;
+        buffer << "fiber " << me << " is started on thread " << thread_id << "\n";
+        std::cout << buffer.str() << std::flush;
     }
 
-    for (int i=0; i<100; ++i) {
-        //boost::this_fiber::yield();
+    for (int i = 0; i < 10; ++i)
+    {
+        // boost::this_fiber::yield();
         boost::this_fiber::sleep_for(100ms);
 
         // энэ fiber-ийг өөр thread дээр шилжүүлсэн эсэхийг илрүүлэх
         std::thread::id new_thread_id = std::this_thread::get_id();
-        if (new_thread_id!=thread_id) {
+        if (new_thread_id != thread_id)
+        {
             thread_id = new_thread_id;
             std::ostringstream buffer;
-            buffer<<"fiber "<<me<<" is switched to thread "<<thread_id<<"\n";
-            std::cout<<buffer.str()<<std::flush;
-        } else {
+            buffer << "fiber " << me << " is switched to thread " << thread_id << "\n";
+            std::cout << buffer.str() << std::flush;
+        }
+        else
+        {
             std::ostringstream buffer;
-            buffer<<"fiber "<<me<<" is running on thread "<<thread_id<<"\n";
-            std::cout<<buffer.str()<<std::flush;
+            buffer << "fiber " << me << " is running on thread " << thread_id << "\n";
+            std::cout << buffer.str() << std::flush;
         }
     }
 
     lock_type lock(mutex_count);
     // fiber ажиллах дуус бүрт fiber_count-н утгыг бууруулах
-    if (0==--fiber_count) {
+    if (0 == --fiber_count)
+    {
         lock.unlock();
         condition_count.notify_all();
     }
 }
 
-
-void worker_thread() {
+void worker_thread()
+{
     std::ostringstream buffer;
-    buffer<<"thread "<<std::this_thread::get_id()<<" is started"<<std::endl;
-    std::cout<<buffer.str()<<std::flush;
+    buffer << "thread " << std::this_thread::get_id() << " is started" << std::endl;
+    std::cout << buffer.str() << std::flush;
 
     boost::fibers::use_scheduling_algorithm<boost::fibers::algo::work_stealing>(4);
 
     lock_type lock(mutex_count);
-    condition_count.wait(lock, []() {
-        return 0==fiber_count;
-    });
+    condition_count.wait(lock, []() { return 0 == fiber_count; });
 }
 
 
-int main() {
-    std::cout<<"main thread "<<std::this_thread::get_id()<<" is started"<<std::endl;
-
-
-    for (char c : std::string("abcdefghijklmnopqrstuvwxyz")) {
-        boost::fibers::fiber([c]() {
-            worker_fiber(c);
+void manager_thread()
+{
+    for (char c : std::string("abcdefghijklmnopqrstuvwxyz"))
+    {
+        boost::fibers::fiber([c]() { 
+            worker_fiber(c); 
         }).detach();
         ++fiber_count;
     }
@@ -91,8 +94,8 @@ int main() {
     std::thread threads[] = {
         std::thread(worker_thread),
         std::thread(worker_thread),
-        std::thread(worker_thread)
-    };
+        std::thread(worker_thread),
+        };
 
     boost::fibers::use_scheduling_algorithm<boost::fibers::algo::work_stealing>(4);
 
@@ -103,17 +106,30 @@ int main() {
         хэрэв дараах нөхцөл биелбэл main fiber дээр resume хийгдэнэ
         өөрөөр хэлбэл бүх fiber-ууд ажиллаж дууссан гэсэн үг
         */
-        condition_count.wait(lock, []() {
-            return 0==fiber_count;
-        });
+        condition_count.wait(lock, [](){ return 0 == fiber_count; });
     }
     // дээрхи lock release хийгдсэний дараа л thread-үүдээ join хийхгүй бол
     // бусад thread-үүд condition_count дотор түгжигдэж deadlock хийгдэх боломжтой
-    for (std::thread & t : threads) {
+    for (std::thread &t : threads)
+    {
         t.join();
     }
+}
 
-    std::cout<<"done, bye."<<std::endl;
+
+
+int main()
+{
+    std::cout << "main thread " << std::this_thread::get_id() << " is started" << std::endl;
+
+    for (int i = 0; i < 2; ++i)
+    {
+        //std::thread manager = std::thread(manager_thread);
+        //manager.join();
+        manager_thread();
+    }
+
+    std::cout << "done, bye." << std::endl;
 
     return 0;
 }
